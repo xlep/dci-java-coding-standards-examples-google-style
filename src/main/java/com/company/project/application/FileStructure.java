1c6187d3f9a66cb26e18c0731793453333ec7317
Philipp Borst <borst.philipp@gmail.com>
2021-07-28 13:12:25 +0200

    added missing Javadoc block

--- src/main/java/com/company/project/application/FileStructure.java
+++ src/main/java/com/company/project/application/FileStructure.java
@@ -2,6 +2,9 @@ package com.company.project.application;
 
 import com.company.project.application.configuration.*;
 
+/**
+ * Checks if the given date fits the current month. Returns true if that is the case.
+ */
 public class FileStructure {
   private final FileStructureConfig fileStructureConfig = new FileStructureConfig();
 }
